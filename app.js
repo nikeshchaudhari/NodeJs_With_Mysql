0c3047d61f7aa79e38b0c520ba1b3db5e0765bbd
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-16 16:39:46 +0545

    Mysql connection

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+
+const dbConnect = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "demo",
+});
+dbConnect.connect((err) => {
+  if (err) {
+    console.log("DB connection failed", err);
+  }
+  console.log("Db connected");
+});
+module.exports = app;
