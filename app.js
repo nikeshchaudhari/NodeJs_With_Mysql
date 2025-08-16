2e2918dd5c8ee041d7a467afe5a238533b0de587
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-16 16:56:56 +0545

    mysql with nodejs

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql2");
-
-const dbConnect = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "demo",
-});
-dbConnect.connect((err) => {
-  if (err) {
-    console.log("DB connection failed", err);
-  }
-  console.log("Db connected");
-});
+const bodyParser = require("body-parser")
+const dataConn = require("./dbconfig")
+app.use(bodyParser.json())
 module.exports = app;
