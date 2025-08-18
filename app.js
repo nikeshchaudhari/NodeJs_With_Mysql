b064c7f5c295f09634227460e727a9f5d316f38b
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-18 17:46:33 +0545

    server

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser")
 const dataConn = require("./dbconfig")
+const userRoute = require("./route/student")
 app.use(bodyParser.json())
+
+app.use("/user",userRoute)
 module.exports = app;
