c8aa2fd4b07b5664d18b047f2aecd98d13591f3d
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-19 14:16:10 +0545

    Dbconnect

---
+++ route/student.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const route = express.Router();
+const db = require("../dbconfig");
+
+route.post("/add-student", async (req, res) => {
+  try {
+    const name = req.body.name;
+    const age = req.body.age;
+    const faculty = req.body.faculty;
+    const query = `INSERT INTO student (name,age,faculty)VALUES(?,?,?)`;
+
+    db.query(query,[name,age,faculty], (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: "Database insert failed" });
+      } else {
+        console.log("Insert Table");
+      }
+       res.status(200).json({
+      msg: "DATA insert..",
+        id: result.insertId,
+    });
+    });
+
+   
+  } catch (err) {
+    console.log("Not insert");
+    res.status(500).json({
+      error: err,
+    });
+  }
+});
+
+module.exports = route;
