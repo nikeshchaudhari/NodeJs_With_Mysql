f91a415e957ea9c0a5f43907f7d3cf2da1a5fd2c
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-19 14:28:52 +0545

    crud

--- route/student.js
+++ route/student.js
@@ -9,25 +9,47 @@ route.post("/add-student", async (req, res) => {
     const faculty = req.body.faculty;
     const query = `INSERT INTO student (name,age,faculty)VALUES(?,?,?)`;
 
-    db.query(query,[name,age,faculty], (err, result) => {
+    db.query(query, [name, age, faculty], (err, result) => {
       if (err) {
         console.log(err);
         return res.status(500).json({ error: "Database insert failed" });
       } else {
         console.log("Insert Table");
       }
-       res.status(200).json({
-      msg: "DATA insert..",
+      res.status(200).json({
+        msg: "DATA insert..",
         id: result.insertId,
+      });
     });
-    });
-
-   
   } catch (err) {
     console.log("Not insert");
     res.status(500).json({
       error: err,
     });
+  }
+});
+
+// mysql get
+
+route.get("/all", async (req, res) => {
+  try {
+    const query = "SELECT * FROM student";
+    db.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result);
+      }
+      res.status(200).json({
+        msg: result
+      });
+    });
+  } catch (err) {
+    console.log("ERRor");
+    res.status(500).json({
+        error:err
+    })
+    
   }
 });
 
